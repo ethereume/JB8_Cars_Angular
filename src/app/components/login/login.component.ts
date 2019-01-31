cd0fc99e0a12b42b8905f871b6301abae64322fb
DeXter1986Golkow <mzawadzki.developer@gmail.com>
2019-01-31 12:32:24 +0100

    Try to rejest by Spring appi

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -13,19 +13,31 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
   public rejest():void {
-
+  	this.loginS.addUser({login:this.login_name,password:this.login_pass,admin:this.isAdmin}).subscribe((dane)=>{
+  			this.parser(dane);
+  	},(err)=>{
+  		console.log(err);
+		this.alerts = err.error.message;
+  	});
   }
   public login():void {
   		if(this.login_name.trim() === "" || this.login_pass.trim() === ""){
   			this.alerts = "Login i hasło nie mogą pyć puste";
   		} else {
 			this.loginS.getUser({login:this.login_name,password:this.login_pass}).subscribe((dane)=>{
-				console.log(dane);
+				this.parser(dane);
   			},(err)=>{
-  				console.log(err);
-  				this.alerts = err.error.error.message;
+  				this.alerts = err.error.message;
   			})
   		}
+  }
+  private parser(dane){
+		let obj = JSON.parse(dane);
+			if(obj.error){
+				this.alerts = obj.error;
+			} else {
+				this.alerts = obj.correct;
+			}
   }
   public login_name:String = "";
   public login_pass:String = "";
--- src/app/components/login/login.service.ts
+++ src/app/components/login/login.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
-
+import { map, take } from 'rxjs/operators'
 /*const httpOptions = {
   headers: new HttpHeaders({ 
     'Access-Control-Allow-Origin':'*',
@@ -17,7 +17,13 @@ export class LoginService {
   constructor(private http: HttpClient) { }
 
   getUser(props) {
-  
-  	return this.http.get(`${this.serverUrl}?login=${props.login}&password=${props.password}`);
+  	return this.http.get(`${this.serverUrl}?login=${props.login}&password=${props.password}`).pipe(map((dane)=>{
+  		return JSON.stringify(dane);
+  	}));
+  }
+  addUser(props){
+  	return this.http.post(this.serverUrl,props).pipe(map((dane)=>{
+  		return JSON.stringify(dane);
+  	}));
   }
 }
